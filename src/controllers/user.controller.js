6ebd4fed88de1d9a4540ff6ef79f16c46a2057dd
Atul <atulkhiyani09@gmail.com>
2025-03-23 20:20:08 +0530

    Register User Completed best practice

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,76 @@
 import {asyncHandler1} from '../utils/asyncHandler.js';
+ import {  validationResult } from 'express-validator';
+import {APIError} from '../utils/ApiError.js';
+import {User} from '../models/user.model.js';
+import {uploadToCloudinary} from '../utils/cloudinary.js';
+import {APIResponse} from '../utils/ApiResponse.js';
 
 const registerUser=asyncHandler1(async(req,res)=>{
+    //taking data from users
+    //validation -> not empty 
+    //already exists
+    //sending img to cloudinary
+    //create user object
+    //saving data to db 
+
+    //YOU CAN USE THIS ALSO
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log( errors.array());
+      return res.status(400).json({ errors: errors.array() });
+    }
+      
+     const {Name,username,email,password}=req.body;
+
+    //you can add more validation here make your own validation using our classs APIError
+//    if(
+//     [username,email,password,Name].some((val)=>val?.trim()==="")//this is the advanced code checking if any of the field is empty return true 
+//    ){
+//     throw new APIError(400,"All Fields are Required");
+//    }
+
+   let existedUser=await User.findOne({//operator to check more things in the DB 
+      $or:[{ username },{ email }]//to check both with or in DB 
+   })
+
+   if(existedUser){
+      throw new APIError(409,"User already exists");
+   }
     
-    
-     res.status(200).json({
-        message:'User registered successfully',
-    })
+   let avatarLocalPath=req.files?.avatar[0]?.path
+   let coverImageLocalPath=req.files?.coverImage[0]?.path
+   if(!avatarLocalPath){
+    throw new APIError(400,"Avatar Img is required");
+   }
+   
+   const avatar=await uploadToCloudinary(avatarLocalPath);
+   const coverImage=await uploadToCloudinary(coverImageLocalPath);
+
+   if(!avatar){
+     throw new APIError(500,"Failed to upload Avatar to Cloud");
+   }
+
+
+   let newUser=await User.create({
+       Name:Name,
+       username:username.toLowerCase(),
+       email:email,
+       password:password,
+       avatar:avatar.url,
+       coverImage:coverImage?.url || ""//check is the url of the img is avaliable if not use empty
+   })
+ 
+   let createdUser=await User.findById(newUser._id).select(
+     "-password -refreshToken" //this is to remove the password and refresh token from the response
+   )
+
+   if(!createdUser){
+    throw new  APIError(500,"Something Went Wrong");
+   }
+   
+    return res.status(201).json(
+        new APIResponse(200,createdUser,"User created successfully")
+    )
 })
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,27 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { body } from 'express-validator';
+import {upload} from '../middlewares/multer.middleware.js'
 const router = Router();
 
-router.route('/register').post(registerUser);
+router.route('/register').post(
+    upload.fields([//uploads.fields to accept the array of the uploaded files from different names
+        {
+            name:'avatar',
+            maxCount:1
+       },
+       {
+         name:'coverImage',
+         maxCount:1
+       }
+    ]),
+    body('username').trim().isLength(3).withMessage("Username must be at least 3 characters"),
+    body('password').trim().isLength(6).withMessage("Password must be at least 6 characters"),
+    body('email').trim().isEmail().withMessage("Email is required"),
+    body("Name").trim().notEmpty().withMessage("Name is required")
+
+    , registerUser
+);
 
 
 
